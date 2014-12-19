2536b1a6aed0b2c65e7706eccabacb97ccb302d4
Gian Merlino <gianmerlino@gmail.com>
2014-12-19 11:18:54 -0800

    Swap stormjar and current-classpath on the worker classpath.

--- storm-core/src/clj/backtype/storm/daemon/supervisor.clj
+++ storm-core/src/clj/backtype/storm/daemon/supervisor.clj
@@ -467,7 +467,7 @@
           jlp (jlp stormroot conf)
           stormjar (supervisor-stormjar-path stormroot)
           storm-conf (read-supervisor-storm-conf conf storm-id)
-          classpath (add-to-classpath (current-classpath) [stormjar])
+          classpath (add-to-classpath stormjar [(current-classpath)])
           worker-childopts (when-let [s (conf WORKER-CHILDOPTS)]
                              (substitute-worker-childopts s port))
           topo-worker-childopts (when-let [s (storm-conf TOPOLOGY-WORKER-CHILDOPTS)]
